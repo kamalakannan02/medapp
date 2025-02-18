abe323509c625621969a54623d20ea70a7d2778a
kamalakannan02 <harish.2207018@srec.ac.in>
2025-02-18 19:21:53 +0530

    modify file

---
+++ script.js
@@ -0,0 +1,16 @@
+// script.js
+
+const signupform = document.getElementById('signupform');
+const signinform = document.getElementById('signinform');
+const togglesignin = document.getElementById('togglesignin');
+const togglesignup = document.getElementById('togglesignup');
+
+togglesignin.addEventListener('click', function() {
+    signupform.style.display = "none";
+    signinform.style.display = "block";
+});
+
+togglesignup.addEventListener('click', function() {
+    signinform.style.display = "none";
+    signupform.style.display = "block";
+});
